7423a05d0b75144f633b9cb3ec48e1de9c4f46d0
priyanshu10 <priyanshuparate@gmail.com>
2024-07-02 06:50:19 +0530

    Program to Check if a Number is Even or Odd

---
+++ src/main/java/basics/P003.java
@@ -0,0 +1,21 @@
+package basics;
+
+import java.util.Scanner;
+
+//Program to Check if a Number is Even or Odd
+public class P003 {
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter number program determine number is even or odd :");
+		int n= sc.nextInt();
+		
+		if(n%2==0) {
+			System.out.println(n+" is Even");
+		}
+		else {
+			System.out.println(n +" is Odd");
+		}
+	}
+
+}
