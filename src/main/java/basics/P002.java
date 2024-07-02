348e753caf42499c649cf5cf4cb6afe172ce4237
priyanshu10 <priyanshuparate@gmail.com>
2024-07-02 06:45:18 +0530

    Program to Print ASCII Value of a Character

---
+++ src/main/java/basics/P002.java
@@ -0,0 +1,22 @@
+package basics;
+
+import java.util.Scanner;
+
+//Program to Print ASCII Value of a Character
+
+public class P002 {
+public static void main(String[] args) {
+
+  Scanner sc= new Scanner(System.in);
+  System.out.println("Enter string ,program print charector and its asci value:");
+  String s=sc.nextLine();
+  char[] stringtochar= s.toCharArray();
+   
+    for(int i =0; i<stringtochar.length;i++) {
+    	
+    	System.out.println(stringtochar[i]+ " : "+(int)stringtochar[i]);
+    }
+
+	
+}
+}
