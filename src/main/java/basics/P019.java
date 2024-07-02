adf2ff5c5133579d7441699f11d534edc331b540
priyanshu10 <priyanshuparate@gmail.com>
2024-07-02 10:47:07 +0530

    Program to Check if a Number is Armstrong

---
+++ src/main/java/basics/P019.java
@@ -0,0 +1,31 @@
+package basics;
+
+import java.util.Scanner;
+
+//Program to Check if a Number is Armstrong
+public class P019 {
+	public static void main(String[] args) {
+		System.out.println("Enter number , program determine the number is armstrong or not");
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		int backup = n;
+
+		int result = 0;
+		while (n > 0) {
+
+			int d = n % 10;
+			n = n / 10;
+			result += Math.pow(d, 3);
+
+		}
+		if (backup == result) {
+			System.out.println(backup + " is Armstrong number");
+		} else {
+			System.out.println(backup + " is not Armstrong number");
+		}
+
+	}
+}
