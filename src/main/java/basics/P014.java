ef31e03aa3998ac8c56cafc1ce6a51bdaee4ac60
priyanshu10 <priyanshuparate@gmail.com>
2024-07-02 09:29:40 +0530

    Program to Find the LCM of Two Numbers

---
+++ src/main/java/basics/P014.java
@@ -0,0 +1,25 @@
+package basics;
+
+import java.util.Scanner;
+
+//Program to Find the LCM of Two Numbers
+public class P014 {
+
+public static void main(String[] args) {
+	System.out.println("Enter 2 number program will determine LCM of 2 number;");
+	
+	Scanner sc= new Scanner(System.in);
+	int a= sc.nextInt();
+	int b=sc.nextInt();
+	
+	int gcd=1;
+	for(int i=1;i<a;i++) {
+		if(a%i==0 && b%i==0) {
+			gcd=i;
+		}
+	}
+	int lcm= a*b/gcd;
+	System.out.println("lcm of "+a +" and "+b+ " is "+lcm);
+}
+	
+}
